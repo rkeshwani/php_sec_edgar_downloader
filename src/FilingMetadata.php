8a7d5aaec2c28af7256e8b80400b6df323bce2d2
rkeshwani <rohit.keshwani@gmail.com>
2023-08-19 19:43:25 +0000

    Created Separate Metadata class and removed reference to autoloader in utils file.

---
+++ src/FilingMetadata.php
@@ -0,0 +1,14 @@
+// Object for storing metadata about filings that will be downloaded.
+class FilingMetadata {
+    public $accession_number;
+    public $full_submission_url;
+    public $filing_details_url;
+    public $filing_details_filename;
+
+    public function __construct($accession_number, $full_submission_url, $filing_details_url, $filing_details_filename) {
+        $this->accession_number = $accession_number;
+        $this->full_submission_url = $full_submission_url;
+        $this->filing_details_url = $filing_details_url;
+        $this->filing_details_filename = $filing_details_filename;
+    }
+}
--- src/Utils.php
+++ src/Utils.php
@@ -1,10 +1,11 @@
 <?php
 namespace jadchaar\secEdgarDownloader;
-require_once __DIR__ . '/../vendor/autoload.php';
+// require_once __DIR__ . '/../vendor/autoload.php';
 
 
 use Exception;
 use jadchaar\secEdgarDownloader\Constants;
+use FilingMetadata;
 
 class Utils
 {
@@ -275,20 +276,5 @@ class Utils
         return count(array_unique(array_map(function ($filing) {
             return $filing->accession_number;
         }, $filings)));
-    }
-}
-
-// Object for storing metadata about filings that will be downloaded.
-class FilingMetadata {
-    public $accession_number;
-    public $full_submission_url;
-    public $filing_details_url;
-    public $filing_details_filename;
-
-    public function __construct($accession_number, $full_submission_url, $filing_details_url, $filing_details_filename) {
-        $this->accession_number = $accession_number;
-        $this->full_submission_url = $full_submission_url;
-        $this->filing_details_url = $filing_details_url;
-        $this->filing_details_filename = $filing_details_filename;
     }
 }
